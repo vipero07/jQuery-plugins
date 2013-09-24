8e6c437b7f3027781fc150e2aa01f314d8771d0a
vipero07 <yodaissmart@gmail.com>
2013-09-24 00:57:30 -0400

    Update jquery.beautify.js
    Increased minification ability, readability, and speed. Added switch for future additions.
    
    http://jsfiddle.net/gHkwh/

--- jquery.beautify.js
+++ jquery.beautify.js
@@ -1,30 +1,41 @@
 (function ($) {
     $.beautify = function (element) {
-        /// <summary>Makes all elements of set type look the same</summary>
+        /// <summary>Makes all elements of set type look the same, and use jqueryui</summary>
         /// <param name="element" type="String">range is the only accepted "element" right now</param>
         /// <returns type="Boolean"></returns>
-        if (element === "range") {
+        var beautifyRange = function () {
             $('input[type="range"]').each(function () {
-                var self = $(this),
-                    slider = $(document.createElement('div'));
-                slider.slider({
-                    range: "min",
-                    value: parseInt(self.val()) || 0,
-                    min: parseInt(self.attr('min')) || 0,
-                    max: parseInt(self.attr('max')) || 100,
-                    step: parseInt(self.attr('step')) || 1,
+                var $self = $(this),
+                    $slider = $(document.createElement('div')),
+                    pInt = parseInt;
+                $slider.slider({
+                    range: 'min',
+                    value: pInt($self.val()) || 0,
+                    min: pInt($self.attr('min')) || 0,
+                    max: pInt($self.attr('max')) || 100,
+                    step: pInt($self.attr('step')) || 1,
                     slide: function (e, ui) {
-                        self.val(ui.value);
+                        $self.val(ui.value);
                     }
                 });
-                self.val(slider.slider("value"))
+                $self.val($slider.slider('value'))
                     .attr('type', 'text')
                     .change(function () {
-                        slider.slider('value', $(this).val());
+                        $slider.slider('value', $self.val());
                     })
-                    .after(slider);
+                    .after($slider);
             });
+        },
+        beautifyAll = function(){
+            beautifyRange();
+        };
+        switch(element){
+            case 'range':
+                beautifyRange();
+                break;
+            default:
+                beautifyAll();
         }
-        return this;
+        return true;
     };
 })(jQuery);

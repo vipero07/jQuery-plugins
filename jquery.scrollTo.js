ee7644d38c0359058877526d75c77d2e0e4b96ab
vipero07 <yodaissmart@gmail.com>
2013-10-09 02:09:42 -0400

    Create jquery.scrollTo.js
    Simple scroll to jquery function

---
+++ jquery.scrollTo.js
@@ -0,0 +1,25 @@
+(function ($) {
+	$.fn.scrollTo = function(options){
+	  /// <summary>Scrolls window to element and calls focus to element, accepts speed in ms, this is chainable</summary>
+    /// <returns type="Object">jquery object</returns>
+		var self = this,
+		    offsets = self.offset(),
+		    settings = {
+		      duration: 2000,
+		      focus: true
+		    };
+		if($.isPlainObject(options)){
+		  $.extend(settings, options);
+		}else if($.isNumeric(options)){
+			settings.duration = options;
+		}
+		$('html, body').animate({
+			scrollTop: offsets.top
+			scrollLeft: offsets.left
+		}, settings.duration);
+		if(settings.focus){
+		  self.focus();
+		}
+		return self;
+	};
+})(jQuery);

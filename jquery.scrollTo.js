2c9a4b86b359a516c56a34ec27c1be811a09bbc7
vipero07 <yodaissmart@gmail.com>
2013-10-10 13:56:24 -0400

    Update jquery.scrollTo.js

--- jquery.scrollTo.js
+++ jquery.scrollTo.js
@@ -1,25 +1,36 @@
 (function ($) {
-	$.fn.scrollTo = function(options){
-	  /// <summary>Scrolls window to element and calls focus to element, accepts speed in ms, this is chainable</summary>
-    /// <returns type="Object">jquery object</returns>
-		var self = this,
-		    offsets = self.offset(),
-		    settings = {
-		      duration: 2000,
-		      focus: true
-		    };
-		if($.isPlainObject(options)){
-		  $.extend(settings, options);
-		}else if($.isNumeric(options)){
-			settings.duration = options;
-		}
-		$('html, body').animate({
-			scrollTop: offsets.top,
-			scrollLeft: offsets.left
-		}, settings.duration);
-		if(settings.focus){
-		  self.focus();
-		}
-		return self;
-	};
+	$.scrollTo = function (options) {
+	        /// <summary>Scrolls window to location defined by top and left</summary>
+	        /// <param name="options" type="Object"></param>
+	        var settings = { duration: 2000, top: 0, left: 0 };
+	        if ($.isPlainObject(options)) {
+	            $.extend(settings, options);
+	        } else if ($.isNumeric(options)) {
+	            settings.duration = options;
+	        }
+	        $('html, body').animate({
+	            scrollTop: settings.top,
+	            scrollLeft: settings.left
+	        }, settings.duration);
+	    };
+	    $.fn.scrollTo = function(options){
+	        /// <summary>Scrolls window to element and calls focus to element, accepts duration in ms, this is chainable</summary>
+	        /// <returns type="Object">jquery object</returns>
+	        var self = this,
+			    offsets = self.offset(),
+			    settings = {
+			        focus: true
+			    };
+	        if($.isPlainObject(options)){
+	            $.extend(settings, options);
+	        }else if($.isNumeric(options)){
+	            settings.duration = options;
+	        }
+	        $.extend(settings, offsets);
+	        $.scrollTo(settings);
+	        if(settings.focus){
+	            self.focus();
+	        }
+	        return self;
+	    };
 })(jQuery);

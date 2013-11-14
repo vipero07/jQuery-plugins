e961b11db02a04742d7713321c2cd58755947c43
vipero07 <yodaissmart@gmail.com>
2013-11-14 18:51:38 -0500

    Update jquery.humanReadable.js
    moved static variables out of the function
    
    fixed short circuit return value to return a string

--- jquery.humanReadable.js
+++ jquery.humanReadable.js
@@ -1,17 +1,16 @@
 (function ($) {
+  var kilo = 1000, suffix = ' kMBTPEZY';
   $.humanReadable = function (number) {
       /// <summary>Formats numbers to be human readable</summary>
       /// <param name="number" type="decimal"></param>
       /// <returns type="String"></returns>
       var retValue = false;
       if (typeof number == "number") {
-          var kilo = 1000;
           if (number < kilo) {
-              retValue = number;
+              retValue = number.toString();
           } else {
-              var s = ' kMBTPEZY',
-                  e = Math.floor(Math.log(number) / Math.log(kilo));
-              retValue = Number((number / Math.pow(kilo, e)).toString().slice(0, 3)) + s.charAt(e);
+              var e = Math.floor(Math.log(number) / Math.log(kilo));
+              retValue = Number((number / Math.pow(kilo, e)).toString().slice(0, 3)) + suffix.charAt(e);
           }
       }
       return retValue;

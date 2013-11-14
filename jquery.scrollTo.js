3c686566b6bb2555689a1b7cc80b980f1840b4d1
vipero07 <yodaissmart@gmail.com>
2013-11-14 18:57:29 -0500

    Update jquery.scrollTo.js
    moved static variables out of functions

--- jquery.scrollTo.js
+++ jquery.scrollTo.js
@@ -1,34 +1,34 @@
 (function ($) {
+	var scrollSettings = { duration: 2000, top: 0, left: 0 };
 	$.scrollTo = function (options) {
 	        /// <summary>Scrolls window to location defined by top and left</summary>
 	        /// <param name="options" type="Object"></param>
-	        var settings = { duration: 2000, top: 0, left: 0 };
+	        
 	        if ($.isPlainObject(options)) {
-	            $.extend(settings, options);
+	            $.extend(scrollSettings, options);
 	        } else if ($.isNumeric(options)) {
-	            settings.duration = options;
+	            scrollSettings.duration = options;
 	        }
 	        $('html, body').animate({
-	            scrollTop: settings.top,
-	            scrollLeft: settings.left
-	        }, settings.duration);
+	            scrollTop: scrollSettings.top,
+	            scrollLeft: scrollSettings.left
+	        }, scrollSettings.duration);
 	    };
+	    
+	    var fnScrollSettings = { focus: true };
 	    $.fn.scrollTo = function(options){
 	        /// <summary>Scrolls window to element and calls focus to element, accepts duration in ms, this is chainable</summary>
 	        /// <returns type="Object">jquery object</returns>
 	        var self = this,
-			    offsets = self.offset(),
-			    settings = {
-			        focus: true
-			    };
+			    offsets = self.offset();
 	        if($.isPlainObject(options)){
-	            $.extend(settings, options);
+	            $.extend(fnScrollSettings, options);
 	        }else if($.isNumeric(options)){
-	            settings.duration = options;
+	            fnScrollSettings.duration = options;
 	        }
-	        $.extend(settings, offsets);
-	        $.scrollTo(settings);
-	        if(settings.focus){
+	        $.extend(fnScrollSettings, offsets);
+	        $.scrollTo(fnScrollSettings);
+	        if(fnScrollSettings.focus){
 	            self.focus();
 	        }
 	        return self;

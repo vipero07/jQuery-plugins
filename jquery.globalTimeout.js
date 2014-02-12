3953695e99aafbda9708fc34492e459b3a573b9c
vipero07 <yodaissmart@gmail.com>
2014-02-12 16:00:42 -0500

    allowed for multiple timeouts
    can now make long or short global timeouts (this is mostly to handle an issue with jquery.timeago setting an interval on every timeago element, and now it can do so smartly.)

--- jquery.globalTimeout.js
+++ jquery.globalTimeout.js
@@ -1,7 +1,7 @@
 (function ($){
 	if(!$.globalTimeout){
 		var defaults = { rate: 200, max: false, name: "timeout" },
-		timeoutVar, $window = $(window), total, clear = false;
+		timeoutVar = [], $window = $(window), total, clear = false;
 		
 		$.globalTimeout = function(options){
 			if($.isNumeric(options)){
@@ -14,26 +14,26 @@
 			
 			total = 0;
 			if(clear){
-				clearTimeout(timeoutVar);
+				clearTimeout(timeoutVar[defaults.name]);
 				clear = false;
 			}else{
-				clearTimeout(timeoutVar);
-				var timeoutFunction;
+				var timeoutFunction, name = defaults.name, rate = defaults.rate, max = defaults.max;
+				clearTimeout(timeoutVar[name]);
 				
-				if(defaults.max){
+				if(max){
 					timeoutFunction = function(){
-						$window.trigger(defaults.name);
+						$window.trigger(name);
 						total++;
-						if(total < defaults.max){
-							timeoutVar = setTimeout(timeoutFunction, defaults.rate);
+						if(total < max){
+							timeoutVar[name] = setTimeout(timeoutFunction, rate);
 						}else{
 							total = 0;
 						}
 					};
 				}else{
 					timeoutFunction = function(){
-						$window.trigger(defaults.name);
-						timeoutVar = setTimeout(timeoutFunction, defaults.rate);
+						$window.trigger(name);
+						timeoutVar[name] = setTimeout(timeoutFunction, rate);
 					};
 				}
 				timeoutFunction();
